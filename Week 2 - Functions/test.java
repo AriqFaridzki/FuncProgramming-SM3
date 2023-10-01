8775f7854442509b7f969b4ef0e993cc71e1cbd8
Muhammad Ariq Faridzki <ariqf35@gmail.com>
2023-10-01 22:34:17 +0700

    nambahin pdf hasil laporan

--- Week 2 - Functions/test.java
+++ Week 2 - Functions/test.java
@@ -1,20 +1,16 @@
-
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
 
 class Functional {
     public static void main(String[] args) {
 
         List<String> data = Arrays.asList("Ariq@gmail.com", "someone@ariq.com", "aere", "lmao", "dickyggaming@gmail.com");
 
-        System.out.println("Declarative way :" + findEmailFuctional(data, "ar"));
-        System.out.println("Imperative way: " + findEmailImperative(data, "ar"));
+        System.out.println(findEmailFuctional(data, "ar"));
+        System.out.println(findEmailImperative(data, "ar"));
 
-        
         };
 
         /**
@@ -26,19 +22,15 @@ class Functional {
          */
         
         static List<String> findEmailFuctional (final List<String> array, final String searchArgs){
-        
-        return array.stream() // array dibuat mengantri(pipeline) satu per satu agar bisa diapply function lain
 
-        .map(string -> string.toLowerCase()) // parameter array dibuat string menjadi huruf kecil agar konsisten
-
+        return array.stream()
+        .map(string -> string.toLowerCase())
         .filter(string -> string.contains("@") && string.contains(searchArgs.toLowerCase()))
-        // jika string yang dicari berisikan "@" dan kriteria data yang dicari
-
         .collect(Collectors.toList());
-        // setelah selesai di filter maka akan di convert kembali menjadi tipe data List<String>
+        
         }
 
-          /**
+        /**
          * Mencari email dengan string biasa - Imperative Way
          * 
          * @param array data emailnya
@@ -46,19 +38,20 @@ class Functional {
          * @return array List tipe String
          */
         static List<String> findEmailImperative (List<String> array, String searchArgs){
-            List<String> result = new ArrayList<>(); // membuat array baru untuk menampung data
+            
+            List<String> result = new ArrayList<>(); 
 
-            for (int i = 0; i < array.size(); i++) { // loop setiap data yang ada di array
-                String email = array.get(i).toLowerCase(); // mengambil data dan diubah menjadi huruf kecil
+            for (int i = 0; i < array.size(); i++) { 
+                String email = array.get(i).toLowerCase(); 
                 if (email.contains("@") && email.contains(searchArgs.toLowerCase())) { 
-                    //jika variable email terdapat "@" dan kriteria data yang dicari
+                    
 
                     result.add(array.get(i).toLowerCase());
-                    // maka tambahkan ke array penampungan (result)
+                    
                 }
             }
             
-            return result; // setelah itu di return setelah loop selesai
+            return result; 
         }
 
 
